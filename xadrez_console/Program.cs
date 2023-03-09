ee4f73a460fefce8b17d2ba93d43e9bcfb4c9294
Thiago Bueno <thiagodeproencabueno@gmail.com>
2023-03-09 09:44:48 -0300

    Criado classe Posição

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using xadrez_console.Tabuleiro;
+
+namespace xadrez_console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
+        }
+    }
+}
